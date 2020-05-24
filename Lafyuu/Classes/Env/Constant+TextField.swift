1515a3b30836e03a29d351796b4eafc3b54ee413
Mikiya Abe <struuuuggle@gmail.com>
2020-05-25 02:02:41 +0900

    Create `LafyuuTextField` for AddCardView

---
+++ Lafyuu/Classes/Env/Constant+TextField.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Mikiya Abe(@struuuuggle) on 2020/05/25.
+//  Copyright © 2020, Mikiya Abe. All rights reserved.
+//
+
+import SwiftUI
+
+extension Constant {
+  enum TextField {
+    static let height: CGFloat = 48
+  }
+}
---
+++ Lafyuu/Classes/Views/Components/LafyuuTextField.swift
@@ -0,0 +1,34 @@
+//
+//  Created by Mikiya Abe(@struuuuggle) on 2020/05/25.
+//  Copyright © 2020, Mikiya Abe. All rights reserved.
+//
+
+import SwiftUI
+
+struct LafyuuTextField: View {
+  private(set) var placeholder: String
+  @State private(set) var text: String
+  var body: some View {
+    TextField(
+      placeholder,
+      text: $text
+    )
+      .font(R.font.poppinsRegular, size: 12)
+      .foregroundColor(.init(R.color.grey))
+      .frame(height: Constant.TextField.height)
+      .offset(x: 16)
+      .roundCorner(with: R.color.light)
+  }
+}
+
+struct LafyuuTextField_Previews: PreviewProvider {
+  @State static var text: String = ""
+  static var previews: some View {
+    LafyuuTextField(
+      placeholder: "Enter Card Number",
+      text: text
+    )
+      .lafyuuPadding()
+      .previewLayout(.fixed(width: 375, height: 60))
+  }
+}
